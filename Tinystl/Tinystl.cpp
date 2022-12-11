64e57cf16f1ba1b10212c96c78f7cb1f211c1942
LPX <3160103983@zju.edu.cn>
2022-12-11 15:41:06 +0800

    Update Tinystl.cpp

--- Tinystl/Tinystl.cpp
+++ Tinystl/Tinystl.cpp
@@ -1,4 +1,5 @@
-
+// Tinystl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
 #include <iostream>
 
 #include "Algorithm.h"
@@ -6,15 +7,19 @@
 #include "Test\StringTest.h"
 #include "Test\ListTest.h"
 #include "Test\Unordered_setTest.h"
+#include "Test\DequeTest.h"
+#include "Test\QueueTest.h"
+#include "Test\StackTest.h"
 
 int main()
 {
-    TinySTL::string s;
-    std::cout << "Hello World!\n";
     TinySTL::Unordered_setTest::testAllCases();
     TinySTL::VectorTest::testAllCases();
     TinySTL::StringTest::testAllCases();
     TinySTL::ListTest::testAllCases();
+    TinySTL::DequeTest::testAllCases();
+    TinySTL::QueueTest::testAllCases();
+    TinySTL::StackTest::testAllCases();
     system("pause");
 }
 
